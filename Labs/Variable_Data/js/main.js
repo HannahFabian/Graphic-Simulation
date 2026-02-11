6828a68af937c31f9acc15da1d51cdd6fd882bbc
HannahFabian <0252139@up.edu.mx>
2026-02-10 21:59:08 -0600

    Add files via upload

---
+++ Labs/Variable_Data/js/main.js
@@ -0,0 +1,23 @@
+/*
+*    main.js
+*/
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 400)
+	.attr("height", 400);
+
+var data = [25, 20, 15, 10, 5];
+
+svg.selectAll("rect")
+	.data(data)
+	.enter()
+	.append("rect")
+	.attr("x", function(d, i) {
+		return i * 50;
+	})
+	.attr("width", 40)
+	.attr("height", function(d) {
+		return d;
+	})
+	.attr("y", function(d) {
+		return 400 - d;
+	});
