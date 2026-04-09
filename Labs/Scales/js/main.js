a723ffc2e17e94e18fcc447ac700da56e3ba4ba4
HannahFabian <0252139@up.edu.mx>
2026-04-09 13:03:44 -0600

    Add files via upload

---
+++ Labs/Scales/js/main.js
@@ -0,0 +1,36 @@
+/*
+*    main.js
+*/
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 500)
+	.attr("height", 500);
+
+d3.json("data/buildings.json").then((data) => {
+	console.log(data);
+
+	data.forEach(d => {
+		d.height = +d.height;
+	});
+
+	const x = d3.scaleBand()
+		.domain(data.map(d => d.name))
+		.range([0, 400])
+		.paddingInner(0.3)
+		.paddingOuter(0.3);
+
+	const y = d3.scaleLinear()
+		.domain([0, 828])
+		.range([400, 0])
+
+	const color = d3.scaleOrdinal(d3.schemeSet3);
+
+	svg.selectAll("rect")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("x", d => x(d.name))
+		.attr("y", d => y(d.height))
+		.attr("width", x.bandwidth())
+		.attr("height", d => 400 - y(d.height))
+		.attr("fill", d => color(d.name));
+})
